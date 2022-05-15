a923aedfb28dc75bacebd2372b8f293c6ee96337
Bobby Lee <superbahbi@users.noreply.github.com>
2022-05-14 22:23:50 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
