0948625de7c6c538d2c34b13282a22f649cdffd7
Bobby Lee <superbahbi@users.noreply.github.com>
2022-05-14 22:24:04 -0700

    Added useWordle hooks

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const useWordle = (solution) => {
+
+    const [turn, setTurn] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([]) // each guess is an array
+    const [history, setHistory] = useState([]) // each guess is a string
+    const [isCorrect, setIsCorrect] = useState(false)
+
+    const formatGuess = () => {
+
+    }
+    const addNewGuess = () => {
+
+    }
+    const handleKeyup = () => {
+
+    }
+}
+export default useWordle
