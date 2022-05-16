a6a5ed73341191d6e7391b13585fa6fc93b6b9ff
Bobby Lee <superbahbi@users.noreply.github.com>
2022-05-15 20:12:37 -0700

    Added current row block

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 
     if (guess) {
         return (
@@ -13,7 +13,22 @@ export default function Row({ guess }) {
             </div>
         )
     }
+    if (currentGuess) {
+        let letters = currentGuess.split('')
 
+        return (
+            <div className="row current">
+                {letters.map((letter, i) =>
+                    <div key={i} className="filled">
+                        {letter}
+                    </div>
+                )}
+                {[...Array(5 - letters.length)].map((_, i) => (
+                    <div key={i}></div>
+                ))}
+            </div>
+        )
+    }
     return (
         <div className="row">
             <div></div>
