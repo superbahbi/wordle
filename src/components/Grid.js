0dea002af253bbc38091b16b15c443ac1c3d4b8b
Bobby Lee <superbahbi@users.noreply.github.com>
2022-05-15 19:37:11 -0700

    Added row styling

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,7 +5,7 @@ export default function Grid({ currentGuess, guesses, turn }) {
     return (
         <div>
             {guesses.map((g, i) => {
-                return <Row key={i} />
+                return <Row key={i} guess={g} />
             })}
         </div>
     )
