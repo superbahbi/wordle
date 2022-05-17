bc632f1c46a561a1fce6f5df9b68958dd1da4de3
Bobby Lee <superbahbi@users.noreply.github.com>
2022-05-17 16:17:46 -0700

    uncledle

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ function App() {
   const [solution, setSolution] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
-      .then(res => res.json())
-      .then(json => {
-        const randomSolution = json[Math.floor(Math.random() * json.length)]
-        setSolution(randomSolution.word)
-      })
+    const data = [ {"id": 1, "word": "edgar"}]
+    const randomSolution = data[Math.floor(Math.random() * data.length)]
+    setSolution(randomSolution.word)
   }, [setSolution]);
   return (
     <div className="App">
-      <h1>Wordle</h1>
+      <h1>Uncledle</h1>
       {solution && <Wordle solution={solution} />}
     </div>
   );
 }
-
 export default App;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Keypad({ usedKeys }) {
-    const [letters, setLetters] = useState(null)
+    const [letters, setLetters] = useState([
+        { "key": "a" },
+        { "key": "b" },
+        { "key": "c" },
+        { "key": "d" },
+        { "key": "e" },
+        { "key": "f" },
+        { "key": "g" },
+        { "key": "h" },
+        { "key": "i" },
+        { "key": "j" },
+        { "key": "k" },
+        { "key": "l" },
+        { "key": "m" },
+        { "key": "n" },
+        { "key": "o" },
+        { "key": "p" },
+        { "key": "q" },
+        { "key": "r" },
+        { "key": "s" },
+        { "key": "t" },
+        { "key": "u" },
+        { "key": "v" },
+        { "key": "w" },
+        { "key": "x" },
+        { "key": "y" },
+        { "key": "z" }
+    ])
 
-    useEffect(() => {
-        fetch('http://localhost:3001/letters')
-            .then(res => res.json())
-            .then(json => {
-                setLetters(json)
-            })
-    }, [])
     return (
         <div className="keypad">
             {letters && letters.map(l => {
