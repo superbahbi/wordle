863b3e15eec092c9a8f9afeb3cee220fa66b2d30
Bobby Lee <superbahbi@gmail.com>
2022-12-02 19:04:08 -0800

    formatting

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,112 +1,123 @@
-import { useState } from "react"
-import data from "../data/db.json"
+import { useState } from "react";
+import data from "../data/db.json";
 
 const useWordle = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([...Array(6)]); // each guess is an array
+  const [history, setHistory] = useState([]); // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys, setUsedKeys] = useState({});
+  const [notInWordList, setNotInWordList] = useState(false);
 
-    const [turn, setTurn] = useState(0)
-    const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
-    const [history, setHistory] = useState([]) // each guess is a string
-    const [isCorrect, setIsCorrect] = useState(false)
-    const [usedKeys, setUsedKeys] = useState({})
-    const [notInWordList, setNotInWordList] = useState(false)
-
-    // Format key with color green, yellow, grey
-    const formatGuess = () => {
-        let solutionArray = [...solution]
-        let formattedGuess = [...currentGuess].map(((l) => {
-            return { key: l, color: "grey" }
-        }))
-        formattedGuess.forEach((l, i) => {
-            if (solutionArray[i] === l.key) {
-                formattedGuess[i].color = "green"
-                solutionArray[i] = null
-            }
-        })
-        formattedGuess.forEach((l, i) => {
-            if (solutionArray.includes(l.key) && l.color !== "green") {
-                formattedGuess[i].color = "yellow"
-                solutionArray[solutionArray.indexOf(l.key)] = null
-            }
-        })
-        return formattedGuess
+  // Format key with color green, yellow, grey
+  const formatGuess = () => {
+    let solutionArray = [...solution];
+    let formattedGuess = [...currentGuess].map((l) => {
+      return { key: l, color: "grey" };
+    });
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray[i] === l.key) {
+        formattedGuess[i].color = "green";
+        solutionArray[i] = null;
+      }
+    });
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray.includes(l.key) && l.color !== "green") {
+        formattedGuess[i].color = "yellow";
+        solutionArray[solutionArray.indexOf(l.key)] = null;
+      }
+    });
+    return formattedGuess;
+  };
+  const addNewGuess = (formattedGuess) => {
+    if (currentGuess === solution) {
+      setIsCorrect(true);
     }
-    const addNewGuess = (formattedGuess) => {
-        if (currentGuess === solution) {
-            setIsCorrect(true)
+    setGuesses((prevGuesses) => {
+      let newGuesses = [...prevGuesses];
+      newGuesses[turn] = formattedGuess;
+      return newGuesses;
+    });
+    setHistory((prevHistory) => {
+      return [...prevHistory, currentGuess];
+    });
+    setTurn((prevTurn) => {
+      return prevTurn + 1;
+    });
+    setUsedKeys((prevUsedKeys) => {
+      let newKeys = { ...prevUsedKeys };
+      formattedGuess.forEach((l) => {
+        const currentColor = newKeys[l.key];
+        if (l.color === "green") {
+          newKeys[l.key] = "green";
+          return;
         }
-        setGuesses((prevGuesses) => {
-            let newGuesses = [...prevGuesses]
-            newGuesses[turn] = formattedGuess
-            return newGuesses
-        })
-        setHistory((prevHistory) => {
-            return [...prevHistory, currentGuess]
-        })
-        setTurn((prevTurn) => {
-            return prevTurn + 1
-        })
-        setUsedKeys((prevUsedKeys) => {
-            let newKeys = { ...prevUsedKeys }
-            formattedGuess.forEach((l) => {
-                const currentColor = newKeys[l.key]
-                if (l.color === "green") {
-                    newKeys[l.key] = "green"
-                    return
-                }
-                if (l.color === "yellow" && currentColor !== "green") {
-                    newKeys[l.key] = "yellow"
-                    return
-                }
-                if (l.color === "grey" && currentColor !== "yellow" && currentColor !== "green") {
-                    newKeys[l.key] = "grey"
-                    return
-                }
-            })
-            return newKeys
-        })
-        setCurrentGuess('')
+        if (l.color === "yellow" && currentColor !== "green") {
+          newKeys[l.key] = "yellow";
+          return;
+        }
+        if (
+          l.color === "grey" &&
+          currentColor !== "yellow" &&
+          currentColor !== "green"
+        ) {
+          newKeys[l.key] = "grey";
+          return;
+        }
+      });
+      return newKeys;
+    });
+    setCurrentGuess("");
+  };
+  const handleKeyUp = ({ key }) => {
+    if (key === "Enter") {
+      // check if guess is in the word list
+      // if (!data.solutions.map((s) => s.word).includes(currentGuess)) {
+      //     console.log("invalid word")
+      //     setNotInWordList(true)
+      //     return
+      // }
+      // only add guess when current guess is less then 5
+      if (turn > 5) {
+        console.log("you used all your guesses");
+        return;
+      }
+      // dont allowed dupe guess
+      if (history.includes(currentGuess)) {
+        console.log("you are already tried that word");
+        return;
+      }
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars long");
+        return;
+      }
+      addNewGuess(formatGuess());
     }
-    const handleKeyUp = ({ key }) => {
-        if (key === 'Enter') {
-            // check if guess is in the word list
-            if (!data.solutions.map((s) => s.word).includes(currentGuess)) {
-                console.log("invalid word")
-                setNotInWordList(true)
-                return
-            }
-            // only add guess when current guess is less then 5
-            if (turn > 5) {
-                console.log("you used all your guesses")
-                return
-            }
-            // dont allowed dupe guess
-            if (history.includes(currentGuess)) {
-                console.log("you are already tried that word")
-                return
-            }
-            // check word is 5 chars long
-            if (currentGuess.length !== 5) {
-                console.log("word must be 5 chars long")
-                return
-            }
-            addNewGuess(formatGuess())
-        }
-        if (key === "Backspace") {
-            setNotInWordList(false)
-            setCurrentGuess((prev) => {
-                return prev.slice(0, -1)
-            })
-            return
-        }
-        if (/^[A-Za-z]$/.test(key)) {
-            if (currentGuess.length < 5) {
-                setCurrentGuess((prev) => {
-                    return prev + key
-                })
-            }
-        }
+    if (key === "Backspace") {
+      setNotInWordList(false);
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
     }
-    return { turn, currentGuess, guesses, isCorrect, usedKeys, notInWordList, handleKeyUp }
-}
-export default useWordle
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        });
+      }
+    }
+  };
+  return {
+    turn,
+    currentGuess,
+    guesses,
+    isCorrect,
+    usedKeys,
+    notInWordList,
+    handleKeyUp,
+  };
+};
+export default useWordle;
